62f9326d230065205f8b35dc1efdbb0e493045bc
unknown <serge.kharkov@gmail.com>
2022-09-28 23:39:56 +0300

    dotenv

---
+++ dotenv.php
@@ -0,0 +1,24 @@
+<?php
+if file_exists(".env"){
+
+	$f = fopen(".env","r");
+
+	while(!feof($f)){
+
+		$ln = trim(fgets($f));
+		
+		$pt = explode("=",$ln,2);
+
+		if(count($pt) == 2){
+
+			$key = trim($pt[0]);
+			$vl = trim($pt[1]);
+
+			if(!empty($key) AND !empty($vl)){
+
+				$_ENV[$key] = $vl;
+			}
+		}
+	}
+}
+?>
--- mpd_api.php
+++ mpd_api.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/env_config.php';
+// require_once __DIR__ . '/env_config.php';
 
 class mpd_api{
 
